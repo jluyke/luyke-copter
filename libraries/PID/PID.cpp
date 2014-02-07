f54bdf819c47bc29959ddfd442bce6e18a3a68c7
jluyke <jordanluyke@gmail.com>
2014-02-06 20:08:11 -0800

    converted doubles to floats, added moving average, added usage

--- libraries/PID/PID.cpp
+++ libraries/PID/PID.cpp
@@ -1,27 +1,36 @@
 #include "PID.h"
 
-void PID::setup(double p_input, double i_input, double d_input)
+PID::PID(double p_input, double i_input, double d_input) 
 {
   p = p_input;
   i = i_input;
   d = d_input;
 }
 
-double PID::compute(double setpoint, double input) // todo: remove derivative kick via dt
+float PID::compute(float setpoint, float input)
 {
   error = setpoint - input;
-  // P
-  p_term = (error/(double)100) * p;
+  error += error * i;
+  // timing - todo: remove derivative kick via dt
+  // current_time = micros();
+  current_time = millis();
+  cycle_time = current_time - previous_time;
+  previous_time = current_time;
+  // p
+  p_term = (error/(float)100) * p;
   p_term = constrain(p_term, -100, 100);
-  // I
-  i_term += (i * error)/(double)100;
-  i_term = constrain(i_term, -50, 50);
-  // D
+  // i
+  i_term += (i * error)/(float)100;
+  i_term = constrain(i_term, -100, 100);
+  // d
   delta = input - last_input;
-  delta = constrain(delta, -800, 800); // gyro value 800 max per multiwii
+  delta = constrain(delta, -800, 800); // "gyro value 800 max" - multiwii
   last_input = input;
-  d_term = (delta/(double)100) * d;
-  d_term = constrain(d_term, -100, 100);
-  //
+  // "add moving average here to reduce noise" - multiwii
+  delta_sum = delta1 + delta2 + delta;
+  delta2 = delta1;
+  delta1 = delta;
+  d_term = (delta_sum/(float)200) * d;
+  d_term = constrain(d_term, -150, 150);
   return p_term + i_term - d_term;
 }
--- libraries/PID/PID.h
+++ libraries/PID/PID.h
@@ -1,19 +1,26 @@
 #ifndef PID_h
 #define PID_h
 
-#include "Arduino.h" //remove later
+#include "Arduino.h"
 
 class PID
 {
   public:
-    void setup(double p_input, double i_input, double d_input);
-    double compute(double setpoint, double input);
+    PID(double p_input, double i_input, double d_input);
+    float compute(float setpoint, float input);
   private:
-    double p, i, d;
-    double p_term, i_term, d_term;
-    double error, delta, last_input;
+    float p, i, d;
+    float p_term, i_term, d_term;
+    float error, delta, last_input;
+    float delta_sum, delta1, delta2;
+    uint32_t current_time, cycle_time, previous_time;
 };
 
 #endif
 
+// References
 // http://brettbeauregard.com/blog/2011/04/improving-the-beginners-pid-introduction/
+
+// Usage
+// PID *pid_acro_pitch = new PID(0.1, 0.005, 1);
+// float total_pitch = pid_acro_pitch->compute(0, -gyro_y);
